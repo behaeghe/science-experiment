8921beeb4b81c07679ed9c27b686e8f5cd4153ea
behaeghe <jean.remy@gmail.com>
2016-02-17 22:39:15 -0500

    This is after the scoping lecture

--- FunctionLecture.R
+++ FunctionLecture.R
@@ -17,3 +17,17 @@ columnmean <- function (x,removeNA=TRUE) {
   
   means
 }
+
+make.power <- function(n){
+  pow <- function(x){
+    x^n    
+  }
+  pow
+}
+f <- function(x){
+  y<-2
+  y^2+ g(x)
+}
+g <- function (x) {
+  x*y
+}
